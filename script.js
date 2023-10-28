18fc550c4b3f57fbb50827f99ab62749b114c02c
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-10-28 20:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function decimalToBinary(num) {
   //Write you code here
-	const binaryNumber = decimalNumber.toString(2);
-  console.log(`Binary representation: ${binaryNumber}`);
+	 return num.toString(2);
 }
 
 window.decimalToBinary = decimalToBinary;
